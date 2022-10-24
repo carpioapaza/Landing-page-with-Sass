fb697f4ada2eac45d163d996c65dbc4bd0ca539d
carpioapaza <rodrigo25096@gmail.com>
2022-10-24 13:34:51 -0500

    fix z index navbar

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ function Main() {
     if (e.target.matches('.header__menu')) {
       $menu = e.target;
       $navbar = e.target.previousElementSibling;
-      $navbar.classList.toggle('reveal');
-      $menu.classList.toggle('reveal');
+      $navbar.classList.toggle('header__navbar--reveal');
+      $menu.classList.toggle('header__menu--reveal');
       console.log($menu, $navbar);
-      if ($menu.classList.contains('reveal')) {
+      if ($menu.classList.contains('header__menu--reveal')) {
         $menu.textContent = 'close';
       } else {
         $menu.textContent = 'menu';
